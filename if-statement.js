164b6aafde4d7fe750d5bbf595513e3b5cf562c2
linfzmla <51199803+linfzmla@users.noreply.github.com>
2020-01-31 15:52:00 -0600

    Create if-statement.js
    Reto 1

---
+++ if-statement.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>if-statement.js</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+
+<script>
+  var fruit;
+  
+  fruit='naranja';
+  
+  if (fruit.length>=5)
+  {
+   console . log ( 'El nombre de la fruta tiene más de cinco caracteres' )
+  }else{
+  console . log( 'El nombre de la fruta tiene cinco caracteres o menos' )
+  }
+</script>
+
+</body>
+</html>
